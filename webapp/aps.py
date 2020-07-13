d73d187a9258bdcc260bc68201dfb0695e5b571c
kooresh akhbari <kooreshakhbari20@hotmail.com>
2020-07-12 23:10:36 -0400

    Login form

--- webapp/aps.py
+++ webapp/aps.py
@@ -1,16 +1,98 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import flask_login
+
 app = Flask(__name__)
+app.secret_key = 'secret string lol'
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+
+
+users = {'': {'password': ''}}
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = request.form['email']
+    if request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return redirect(url_for('upload_file'))
+
+    return self.login_manager.unauthorized()
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
+
+@app.route('/')
+def hello_world():
+    return redirect(url_for('login'))
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
 
 @app.route('/upload')
+@flask_login.login_required
 def upload_file():
-    return render_template('/upload.html')
-
+   return render_template('/upload.html')
+	
 @app.route('/uploader', methods = ['GET', 'POST'])
+@flask_login.login_required
 def upload_file1():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+		
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=5003, debug = True)
+   app.run(debug = True)
